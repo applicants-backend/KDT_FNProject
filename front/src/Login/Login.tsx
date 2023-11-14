dd88dec0eab688ed11ca146e52fe48663d89a818
applicants-backend <tyser@naver.com>
2023-11-14 17:11:04 +0900

    Login start

---
+++ front/src/Login/Login.tsx
@@ -0,0 +1,24 @@
+import {useState} from 'react'
+import axios, { Axios, AxiosResponse } from 'axios'
+import styled from 'styled-components'
+
+interface LoginRespinse {
+    
+}
+
+const LoginContainer = styled.div `
+width: 500px;
+height: 500px;
+
+`
+
+  
+export default function Login() {
+
+    return
+            <>
+                <div className='LoginContainer'>
+
+                </div>
+            </>
+}
