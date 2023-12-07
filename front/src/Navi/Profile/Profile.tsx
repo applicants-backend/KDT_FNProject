1b0a1860ae941053fb8f6ec56acf686bfd43c154
kangseokjooo <zpckq69@naver.com>
2023-12-07 17:49:12 +0900

    style:invitecode

--- front/src/Navi/Profile/Profile.tsx
+++ front/src/Navi/Profile/Profile.tsx
@@ -1,29 +1,30 @@
-import { useEffect, useState, SyntheticEvent,FC } from "react"
+import { useEffect, useState, SyntheticEvent, FC } from "react"
 import UserTypeState, { ProfileState, URLstate, UserDataState, WorkerListState } from "../../Store/Store"
 import axios from "axios"
 import "./scss/Profile.scss"
-
 import ReactModal from "react-modal"
 import inviteModalStyles from './scss/InviteModal';
 
 interface profileProps {
-    setmodalOpenis?:React.Dispatch<React.SetStateAction<boolean>>;
-    logout? : Function
+    setmodalOpenis?: React.Dispatch<React.SetStateAction<boolean>>;
+    logout?: Function
 }
 
-export default function Profile ({setmodalOpenis, logout}:profileProps):ReturnType<FC> {
+export default function Profile({ setmodalOpenis, logout }: profileProps): ReturnType<FC> {
 
-    const {URL} = URLstate(state=>state)
-    const {UserType} = UserTypeState(state=>state)
-    const {Memberid, Token, Storeid, setToken, setName} = UserDataState(state=>state)
-    const {userImg,companyImg,name,phonenumber,companyNumber,companyName, setuserImg, setcompanyImg, setname, setphonenumber, setcompanyName, setcompanyNumber} = ProfileState(state=>state)
-    const {setWorkList,WorkerList} = WorkerListState(state=>state)
+    const { URL } = URLstate(state => state)
+    const { UserType } = UserTypeState(state => state)
+    const { Memberid, Token, Storeid, setToken, setName } = UserDataState(state => state)
+    const { userImg, companyImg, name, phonenumber, companyNumber, companyName, setuserImg, setcompanyImg, setname, setphonenumber, setcompanyName, setcompanyNumber } = ProfileState(state => state)
+    const { setWorkList, WorkerList } = WorkerListState(state => state)
 
     const [inviteModal, setInviteModal] = useState<boolean>(false);
+    const [showToken, setShowToken] = useState<boolean>(false);
 
-    useEffect(()=> {
+    useEffect(() => {
+    
         const loadUserData = async () => {
-            const UserRes = await axios.post(`${URL}/detail`,{memberid :Memberid})
+            const UserRes = await axios.post(`${URL}/detail`, { memberid: Memberid })
             const Userprofile = UserRes.data.data.member
             const Storeprofile = UserRes.data.data.store
 
@@ -36,106 +37,99 @@ export default function Profile ({setmodalOpenis, logout}:profileProps):ReturnTy
             setcompanyNumber(Storeprofile.companynumber)
             setcompanyImg(Storeprofile.companyimg)
             setToken(Storeprofile.invitecode)
-            console.log(UserRes)
-            if(UserType === 'admin'){
+
+            if (UserType === 'admin') {
                 const WorkerListRes = await axios.get(`${URL}/admin/attendance/workerlist/${Memberid}/${Storeid}`)
                 setWorkList(WorkerListRes.data.data)
-                console.log(WorkerList)
-            }     
+            }
         }
         loadUserData()
-    },[Memberid,URL,companyImg])
-
-  
+    }, [Memberid, URL, companyImg])
 
     const editProfle = () => {
-
         if (setmodalOpenis) setmodalOpenis(true)
     }
 
-    const CodeGenerater = async () => {
-        const CodeRes = await axios.post(`${URL}/generate`,{companynumber : companyNumber})
-        const Code = CodeRes.data
-        setToken(Code)
-        console.log(Code)
+    const toggleToken = () => {
+        setShowToken(!showToken);
+        if (!showToken) CodeGenerater();
     }
 
+    const CodeGenerater = async () => {
+        const CodeRes = await axios.post(`${URL}/generate`, { companynumber: companyNumber })
+        const Code = CodeRes.data
+        setToken(Code)
+    }
 
-    const defalutImg = (e:SyntheticEvent<HTMLImageElement, Event> | any) => {
+    const defalutImg = (e: SyntheticEvent<HTMLImageElement, Event> | any) => {
         e.currentTarget.src = "https://kdt9hotdog.s3.ap-northeast-2.amazonaws.com/alba/defalut_image.png";
     }
 
     return (
-        
         <div className="profile">
             <div className="profile-line"></div>
             <div className="profile-img">
-                
-                <img src={UserType ==="admin" ? companyImg === null ? "" : companyImg : userImg === null ? "" : userImg } 
-                        alt='profile-image' 
-                        onError={defalutImg}/>
+                <img
+                    src={UserType === "admin" ? companyImg === null ? "" : companyImg : userImg === null ? "" : userImg}
+                    alt='profile-image'
+                    onError={defalutImg}
+                />
             </div>
-            
+
             <div className="profile-info">
                 <p className="profile-name"> {name}</p>
-                {/* <p> {phonenumber}</p> */}
                 <p className="profile-companyName"> {companyName}</p>
-                {/* {UserType === "admin" ?  <p> Number : {companyNumber}</p> : <></>} */}
             </div>
+
             <div className="profile-line"></div>
             <div className="profile-icon-box" >
-                { UserType === "admin" ? 
-                    <div className="invite-icon" onClick={(e)=>{setInviteModal(true)}}>
-                      <span className="material-symbols-outlined">vpn_key</span>
-                        {/* <img src={"https://kdt9hotdog.s3.ap-northeast-2.amazonaws.com/alba/invitation_icon.png"} alt="icon" /> */}
+                {UserType === "admin" ?
+                    <div className="invite-icon" onClick={(e) => { setInviteModal(true) }}>
+                        <span className="material-symbols-outlined">vpn_key</span>
                     </div>
                     :
                     <></>
                 }
-                <div className="profile-icon" onClick={(e)=>{editProfle()}}>
+                <div className="profile-icon" onClick={(e) => { editProfle() }}>
                     <span className="material-symbols-outlined">settings</span>
-                    {/* <img src={"https://kdt9hotdog.s3.ap-northeast-2.amazonaws.com/alba/edit_icon.png"} alt="edit_icon" /> */}
                 </div>
-                <div className="logout-icon" onClick={() => {if (logout) logout()}}>
-                <span className="material-symbols-outlined">logout</span>
-                    {/* <img src={"https://kdt9hotdog.s3.ap-northeast-2.amazonaws.com/alba/shutdown_icon.png"} alt="shutdown_icon" /> */}
+                <div className="logout-icon" onClick={() => { if (logout) logout() }}>
+                    <span className="material-symbols-outlined">logout</span>
                 </div>
             </div>
-          
+
             <ReactModal
-                    ///// modal 설정
-                    isOpen={inviteModal}
-                    onRequestClose={()=> setInviteModal(false)}
-                    overlayClassName= "invite-modal"
-                    ariaHideApp={false}
-                    shouldCloseOnOverlayClick={true}
-                    style={inviteModalStyles}
-                >
+                isOpen={inviteModal}
+                onRequestClose={() => setInviteModal(false)}
+                overlayClassName="invite-modal"
+                ariaHideApp={false}
+                shouldCloseOnOverlayClick={true}
+                style={inviteModalStyles}
+            >
                 <div style={{
-                    width : "100%",
+                    width: "100%",
                     height: "100%",
-                    display:"flex"
-                }}>
-                <div style={{
-                    width:"100%",
-                    height:"100%",
-                    margin:"auto",
-                    display:"flex",
-                    flexDirection:"column",
-                    textAlign:"center",
-                    justifyContent:"center",
-                    alignItems:"center"
+                    display: "flex"
                 }}>
                     <div style={{
+                        width: "100%",
+                        height: "100%",
+                        margin: "auto",
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign: "center",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                        <div style={{
                             width: "100%",
                             height: "50px",
-                    }}>
-                        {Token}
+                        }}>
+                            {showToken && Token}
+                        </div>
+                        <button className="codeGenerator" style={{ width: "30%", padding: "3%", color: "rgb(85, 105, 255)", border: "1px solid rgb(85, 105, 255)", backgroundColor: "white", borderRadius: "10px" }} onClick={toggleToken}>{showToken ? '초대코드 숨김' : '초대코드 발급'}</button>
+                        <div onClick={() => setInviteModal(false)} style={{ position: "absolute", top: "3%", right: "3%", fontSize: "24px", cursor: "pointer" }}>X</div>
                     </div>
-                    <button className="codeGenerator" style={{width:"30%",padding:"3%",color:"rgb(85, 105, 255)",border:"1px solid rgb(85, 105, 255)",backgroundColor:"white",borderRadius:"10px"}} onClick={(e)=>CodeGenerater()}>초대코드 발급</button>
-                    <div onClick={()=>setInviteModal(false)} style={{position:"absolute", top:"3%",right:"3%",fontSize:"24px",cursor:"pointer"}}>X</div>
-                </div>
-        
                 </div>
             </ReactModal>
 
